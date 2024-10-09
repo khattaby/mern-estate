1bca74a1b21b4d3e4bf9fc1258c82b0ee46ab54d
khattaby <aymanibrahim678@gmail.com>
2024-10-09 18:03:07 +0300

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// Define the schema
+const userSchema = new mongoose.Schema(
+    username: {
+    type: String,
+    required: true,
+    unique: true,
+},
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+},
+    password: {
+    type: String,
+    required: true,
+}, { timestamps: true }
+);
+
+// Create the model using the schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
