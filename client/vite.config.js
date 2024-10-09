88b45900f8c58856c08d47e90775843b7939a948
khattaby <aymanibrahim678@gmail.com>
2024-10-09 23:19:31 +0300

    complete sign up page funcionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {  // Ensure this starts with a leading slash
+        target: 'http://localhost:3000',
+        secure: false,
+        changeOrigin: true, // Add this to change the origin of the host header to the target URL
+      },
+    },
+  },
   plugins: [react()],
-})
+});
