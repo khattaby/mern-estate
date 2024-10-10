e3d13fbcee7c7bea76f40586d67c6f4b1449fa93
khattaby <aymanibrahim678@gmail.com>
2024-10-10 11:32:11 +0300

    add redux toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './components/Header'
 
+
 export default function App() {
   return (
     <BrowserRouter>
